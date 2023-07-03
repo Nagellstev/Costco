4633e2ae2287c7dcb442f396686557f772bbd109
Vasilii Denisov <v.denisov27@gmail.com>
2023-07-04 02:19:13 +0400

    Added Screenshotter

---
+++ Costco.Utilities/Screenshotter/Screenshotter.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace Costco.Utilities.Screenshotter
+{
+    public static class Screenshotter
+    {
+        private static string? filePath;
+
+        public static void Init(string path)
+        {
+            filePath = path;
+            Directory.CreateDirectory(filePath);
+        }
+
+        public static void TakeScreenshot(IWebDriver driver)
+        {
+            DateTime dateTime = DateTime.Now;
+            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            screenshot.SaveAsFile(Path.Combine(filePath, $"{GetTimeStamp(dateTime)}.jpeg"));
+        }
+
+        private static string GetTimeStamp(DateTime dateTime) => dateTime.ToString("yyyy-MM-dd_HH-mm-ss");
+    }
+}
