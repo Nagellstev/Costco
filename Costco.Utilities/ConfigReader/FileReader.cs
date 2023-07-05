5f2a78d918556e27bd3003b3b6ba06f78991aff6
Filipp <ardos127@gmail.com>
2023-07-05 16:05:18 +0300

    Changed config file according to feedback

--- Costco.Utilities/ConfigReader/FileReader.cs
+++ Costco.Utilities/ConfigReader/FileReader.cs
@@ -6,7 +6,7 @@
 
         /// <summary>
         /// Checks location provided in path and deserializes the specified file.
-        /// Supports xml and json. If working with xml pass the name of the desired node to TargetXmlNode property.  
+        /// Supports xml and json. When working with xml pass the name of the desired node to TargetXmlNode property.  
         /// </summary>
         /// <returns>Contents of the specified file as TModel object.</returns>
         public TModel Read(string path)
