0613e81b32bc1424b55e354d5c2464032cb823d5
Uladzislau <tarasenko_vlad@inbox.ru>
2023-07-07 13:11:18 +0400

    Add BasePage class

---
+++ Costco.Core/BasePage/BasePage.cs
@@ -0,0 +1,31 @@
+﻿using Costco.Core.Browser;
+using Costco.Utilities.Logger;
+using OpenQA.Selenium;
+
+namespace Costco.Core.BasePage
+{
+    public class BasePage
+    {
+
+        private IWebDriver _driver;
+        public string Url { get; }
+
+        public bool IsElementVisible(By locator)
+        {
+            try
+            {
+                return _driver.FindElement(locator).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                Logger.Error("Element wasn't found");
+                return false;
+            }
+        }
+
+        public bool IsPageOpened()
+        {
+            return BrowserFactory.Browser.GetUrl().Equals(Url);
+        }
+    }
+}
