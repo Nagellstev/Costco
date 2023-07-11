326b218b34a7b2e5d93ce275fecd02332f0e68cd
Uladzislau <tarasenko_vlad@inbox.ru>
2023-07-11 19:26:45 +0400

    Create MainPage and LoginPage classes

---
+++ Costco.Web/Pages/LoginPage.cs
@@ -0,0 +1,15 @@
+﻿using Costco.Core.BasePage;
+using Costco.Web.Elements;
+using OpenQA.Selenium;
+
+namespace Costco.Web.Pages
+{
+    public class LoginPage : BasePage
+    {
+        public InputField UsernameInputField => new InputField(By.CssSelector("#signInName"));
+        public InputField PasswordInputField => new InputField(By.XPath("//input[@id='password']"));
+        public Button LoginButton => new Button(By.XPath("//button[@type='submit']"));
+        public TextBox PasswordIsRequiredError => new TextBox(By.XPath("//div[@aria-hidden='false'][@class='error itemLevel']"));
+        public TextBox InvalidCredentialsError => new TextBox(By.XPath("//div[@aria-hidden='false'][@class='error pageLevel']"));
+    }
+}
---
+++ Costco.Web/Pages/MainPage.cs
@@ -0,0 +1,13 @@
+﻿using Costco.Core.BasePage;
+using Costco.Web.Elements;
+using OpenQA.Selenium;
+
+namespace Costco.Web.Pages
+{
+    public class MainPage : BasePage
+    {
+        public override string Url => "https://www.costco.com/";
+        public Button SignInButton => new Button(By.CssSelector("#header_sign_in.myaccount"));
+        public Button AccountButton => new Button(By.CssSelector("#myaccount-react-d"));
+    }
+}
