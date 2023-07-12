d21c87b942fdce041d2696fa1ce1bef99be82427
Uladzislau <tarasenko_vlad@inbox.ru>
2023-07-12 12:28:10 +0400

    Add LoginPage and MainPage

--- Costco.Web/Pages/LoginPage.cs
+++ Costco.Web/Pages/LoginPage.cs
@@ -6,10 +6,16 @@ namespace Costco.Web.Pages
 {
     public class LoginPage : BasePage
     {
-        public InputField UsernameInputField => new InputField(By.CssSelector("#signInName"));
-        public InputField PasswordInputField => new InputField(By.XPath("//input[@id='password']"));
-        public Button LoginButton => new Button(By.XPath("//button[@type='submit']"));
-        public TextBox PasswordIsRequiredError => new TextBox(By.XPath("//div[@aria-hidden='false'][@class='error itemLevel']"));
-        public TextBox InvalidCredentialsError => new TextBox(By.XPath("//div[@aria-hidden='false'][@class='error pageLevel']"));
+        public By UsernameInputFieldLocator = By.CssSelector("#signInName");
+        public By PasswordInputFieldLocator = By.XPath("//input[@id='password']");
+        public By LoginButtonLocator = By.XPath("//button[@type='submit']");
+        public By PasswordIsRequiredErrorLocator = By.XPath("//div[@aria-hidden='false'][@class='error itemLevel']");
+        public By InvalidCredentialsErrorLocator = By.XPath("//div[@aria-hidden='false'][@class='error pageLevel']");
+
+        public InputField UsernameInputField => new InputField(UsernameInputFieldLocator);
+        public InputField PasswordInputField => new InputField(PasswordInputFieldLocator);
+        public Button LoginButton => new Button(LoginButtonLocator);
+        public TextBox PasswordIsRequiredError => new TextBox(PasswordIsRequiredErrorLocator);
+        public TextBox InvalidCredentialsError => new TextBox(InvalidCredentialsErrorLocator);
     }
 }
--- Costco.Web/Pages/MainPage.cs
+++ Costco.Web/Pages/MainPage.cs
@@ -6,8 +6,10 @@ namespace Costco.Web.Pages
 {
     public class MainPage : BasePage
     {
+        public By SignInButtonLocator = By.CssSelector("#header_sign_in.myaccount");
+        public By DisabledSignInButtonLocator = By.XPath("//a[@class='myaccount'][@style='display: none;']");
         public override string Url => "https://www.costco.com/";
-        public Button SignInButton => new Button(By.CssSelector("#header_sign_in.myaccount"));
-        public Button HiddenSignInButton => new Button(By.XPath("//a[@class='myaccount'][@style='display: none;']"));
+        public Button SignInButton => new Button(SignInButtonLocator);
+        public Button DisabledSignInButton => new Button(DisabledSignInButtonLocator);
     }
 }
