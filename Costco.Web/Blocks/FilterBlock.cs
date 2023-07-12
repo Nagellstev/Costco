d20e82a826028dbf4dbf784fda6d123392cc4714
PavelD <nagellstev@ya.ru>
2023-07-12 18:56:16 +0400

    corrected locators

--- Costco.Web/Blocks/FilterBlock.cs
+++ Costco.Web/Blocks/FilterBlock.cs
@@ -6,7 +6,7 @@ namespace Costco.Web.Blocks
     public class FilterBlock : BaseBlock
     {
         private static readonly By filterBlockLocator = By.Id("search-filter");
-        public By PriceCheckBox0to25Locator => By.XPath("//button[@id = 'accordion-filter_collapse-1']//label//span[text('$0 to $25')]");
+        public By PriceCheckBox0to25Locator => By.XPath("//div[@id = 'accordion-filter_collapse-1']//span[contains(text(),'$0 to $25')]");
         public CheckBox PriceCheckBox0to25 => new CheckBox(PriceCheckBox0to25Locator);
         public FilterBlock() : base(filterBlockLocator) { }
     }
