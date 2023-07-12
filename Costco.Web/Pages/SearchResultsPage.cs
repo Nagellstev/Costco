2fe875cc59e5d216138db0d78c42562778d08ae2
PavelD <nagellstev@ya.ru>
2023-07-12 14:32:10 +0400

    created steps

---
+++ Costco.Web/Pages/SearchResultsPage.cs
@@ -0,0 +1,15 @@
+﻿using Costco.Web.Elements;
+using OpenQA.Selenium;
+
+namespace Costco.Web.Pages
+{
+    public class SearchResultsPage : BasePage
+    {
+        public By SearchResultsMessageLocator => By.Id("rsltCntMsg");
+        public TextBox SearchResultsMessage => new TextBox(SearchResultsMessageLocator);
+        public By PriceCheckBox0to25Locator => By.XPath("//button[@id = 'accordion-filter_collapse-1']//label//span[text('$0 to $25')]");
+        public CheckBox PriceCheckBox0to25 => new CheckBox(PriceCheckBox0to25Locator);
+        public By TotalProductsShowingQuantityLocator => By.CssSelector("span[automation-id='totalProductsOutputText']");
+        public TextBox TotalProductsShowingQuantity => new TextBox(TotalProductsShowingQuantityLocator);
+    }
+}
---
+++ Costco.Web/Pages/SearchSteps.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Costco.Web.Pages
+{
+    public class SearchSteps
+    {
+        private SearchResultsPage searchResultsPage;
+        private MainPage mainPage;
+
+        public SearchSteps() 
+        { 
+            mainPage = new MainPage(); 
+            searchResultsPage = new SearchResultsPage();
+        }
+
+        public void SearchFieldInput(string input)
+        {
+            mainPage.SearchField.SendKeys(input);
+            mainPage.SearchField.Submit();
+        }
+
+        public string ReadSearchResultsMessage()
+        {
+            return searchResultsPage.SearchResultsMessage.Text;
+        }
+
+        public void FilterByPrice0to25()
+        {
+            searchResultsPage.PriceCheckBox0to25.Click();
+        }
+
+        public int CheckTotalQuantity()
+        {
+            string str = searchResultsPage.TotalProductsShowingQuantity.Text;
+            return ExtractTotalQuantity(str);
+        }
+
+        private int ExtractTotalQuantity(string text)
+        {
+            //regex extracts number from the end of the string
+            Regex regex = new Regex(@"\d{1,}$");
+            Match match = regex.Match(text);
+            return int.Parse(match.Value);
+        }
+    }
+}
