4d75ff395ad191c4895fd0ade916bb5eb133e0a3
Uladzislau <tarasenko_vlad@inbox.ru>
2023-07-12 16:02:13 +0400

    Add LoginCredentials.json file and LoginCredentialsModel class

---
+++ Costco.TestData/Models/LoginCredentialsModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Costco.TestData.Models
+{
+    public class LoginCredentialsModel
+    {
+        public class Root
+        {
+            public ValidCredentials ValidCredentials { get; set; }
+            public InvalidCredentials InvalidCredentials { get; set; }
+        }
+
+        public class InvalidCredentials
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class ValidCredentials
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
