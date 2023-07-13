79027a74afb8200b164a66127558aa08b2235ab7
Filipp <ardos127@gmail.com>
2023-07-13 19:04:45 +0300

    Test foundation

---
+++ Costco.Tests/CostcoProductPageTestSuite.cs
@@ -0,0 +1,79 @@
+using Costco.Core.Browser;
+using Costco.Utilities.FileReader.Models;
+using Costco.Utilities.Logger;
+using Costco.Utilities.Screenshotter;
+using Costco.Web.Pages;
+
+namespace Costco.Tests
+{
+    public class CostcoProductPageTestSuite : IClassFixture<TestFixture>
+    {
+        TestFixture fixture;
+
+        public CostcoProductPageTestSuite(TestFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void ZeroProductsReturnError()
+        {
+            try 
+            {
+                ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[0]);
+                BrowserFactory.Browser.GoToUrl(productPage.Url);
+                productPage.InputProductAmount("0");
+                productPage.AddToCartButton.Click();
+                Assert.Equal("Quantity must be 1 or more to add to cart.", productPage.GetErrorText());
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Test failed, exception {ex.Message}");
+                Screenshotter.TakeScreenshot(Browser.Driver); 
+                throw;
+            }
+        }
+
+        [Fact]
+        public void OverLimitProductsReturnError()
+        {
+            try
+            {
+                ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[1]);
+                BrowserFactory.Browser.GoToUrl(productPage.Url);
+
+                productPage.InputProductAmount("16");
+                productPage.AddToCartButton.Click();
+                Assert.Contains("", productPage.GetErrorText());
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Test failed, exception {ex.Message}");
+                Screenshotter.TakeScreenshot(Browser.Driver);
+                throw;
+            }
+        }
+
+        [Fact]
+        public void OverMaximumProductReturnError()
+        {
+            try
+            {
+                ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[2]);
+                BrowserFactory.Browser.GoToUrl(productPage.Url);
+                productPage.InputProductAmount("999");
+                productPage.PlusStepper.Click();
+                productPage.AddToCartButton.Click();
+                Assert.Equal("Please enter no more than 3 characters", productPage.GetInputFieldErrorText());
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Test failed, exception {ex.Message}");
+                Screenshotter.TakeScreenshot(Browser.Driver);
+                throw;
+            }
+        }
+    }
+}
