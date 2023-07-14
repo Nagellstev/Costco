25db24eff747e8ba596a75a3f0040dae145823d3
PavelD <nagellstev@ya.ru>
2023-07-14 18:46:38 +0400

    tests pass if run them one by one manually and fail if run the whole suite

--- Costco.Tests/SearchTestSuite.cs
+++ Costco.Tests/SearchTestSuite.cs
@@ -15,20 +15,23 @@ namespace Costco.Tests
             this.fixture = fixture;
         }
 
-        //EPMFARMATS-17486 Search existing item
-        //1. Go to https://www.costco.com/ page
-        //2. Send keys «vitamin» to the search field
-        //result. Header of search results contains substring «vitamin»
+        /// <summary>
+        ///EPMFARMATS-17486 Search existing item
+        ///1. Go to https://www.costco.com/ page
+        ///2. Send keys «vitamin» to the search field
+        ///result. Header of search results contains substring «vitamin»
+        /// </summary>
         [Fact]
         public void SearchExistingItem()
         {
             try
             {
-                MainPage mainPage = new();
+                MainPage mainPage = new MainPage();
                 SearchResultsPage searchResultsPage = new SearchResultsPage();
 
                 string searchString = ((SearchStringModel)fixture.testData).SearchString[0];
                 mainPage.SearchFieldInput(searchString);
+                Waiters.WaitForCondition(() => searchResultsPage.SearchResultsMessage.IsDisplayed(), 5);                
                 string searchResult = searchResultsPage.ReadSearchResultsMessage();
 
                 Assert.Contains(searchString.ToLower(), searchResult.ToLower());
@@ -42,25 +45,30 @@ namespace Costco.Tests
 
         }
 
-        //EPMFARMATS-17487 Search existing item and sort by parameters
-        //1. Go to https://www.costco.com/ page
-        //2. Send keys «vitamin» to the search field
-        //3. Extract total quantity from string "showing 1-XX from YY"
-        //4. Choose $0 to $25 option in "Price" filter area
-        //5. Extract total quantity from string "showing 1-XX from ZZ"
-        //result. Total quantity is updated
+        /// <summary>
+        ///EPMFARMATS-17487 Search existing item and sort by parameters
+        ///1. Go to https://www.costco.com/ page
+        ///2. Send keys «vitamin» to the search field
+        ///3. Extract total quantity from string "showing 1-XX from YY"
+        ///4. Choose $0 to $25 option in "Price" filter area
+        ///5. Extract total quantity from string "showing 1-XX from ZZ"
+        ///result. Total quantity is updated
+        /// </summary>
         [Fact]
         public void SearchExistingItemAndSortByParameters()
         {
             try
             {
-                MainPage mainPage = new();
+                MainPage mainPage = new MainPage();
                 SearchResultsPage searchResultsPage = new SearchResultsPage();
 
                 string searchString = ((SearchStringModel)fixture.testData).SearchString[0];
                 mainPage.SearchFieldInput(searchString);
+                Waiters.WaitForCondition(() => searchResultsPage.TotalProductsShowingQuantity.IsDisplayed(), 5);
                 int totalQuantity = searchResultsPage.CheckTotalQuantity();
                 searchResultsPage.FilterByPrice0to25();
+                Waiters.WaitForPageLoad();
+                Waiters.WaitForCondition(() => searchResultsPage.TotalProductsShowingQuantity.IsDisplayed(), 5);
                 int totalQuantityFiltered = searchResultsPage.CheckTotalQuantity();
 
                 Assert.NotEqual(totalQuantityFiltered, totalQuantity);
@@ -73,20 +81,23 @@ namespace Costco.Tests
             }
         }
 
-        //EPMFARMATS-17488 Search senseless line
-        //1. Go to https://www.costco.com/ page
-        //2. Send keys «qwerty» to the search field
-        //result. Header of search results contains substring «we were not able to find a match»
+        /// <summary>
+        ///EPMFARMATS-17488 Search senseless line
+        ///1. Go to https://www.costco.com/ page
+        ///2. Send keys «qwerty» to the search field
+        ///result. Header of search results contains substring «we were not able to find a match»
+        /// </summary>
         [Fact]
         public void SearchSenselessLine()
         {
             try
             {
-                MainPage mainPage = new();
+                MainPage mainPage = new MainPage();
                 SearchResultsPage searchResultsPage = new SearchResultsPage();
 
                 string searchString = ((SearchStringModel)fixture.testData).SearchString[1];
                 mainPage.SearchFieldInput(searchString);
+                Waiters.WaitForCondition(() => searchResultsPage.SearchResultsMessage.IsDisplayed(), 5);
                 string searchResult = searchResultsPage.ReadSearchResultsMessage();
 
                 Assert.Contains("we were not able to find a match", searchResult.ToLower());
