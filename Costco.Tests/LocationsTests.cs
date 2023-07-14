c17374078f2285bdfe538ad2f92ceb616bdbeba5
Vasilii Denisov <v.denisov27@gmail.com>
2023-07-14 15:57:20 +0400

    Added config

--- Costco.Tests/LocationsTests.cs
+++ Costco.Tests/LocationsTests.cs
@@ -1,8 +1,10 @@
 ﻿using Costco.Core.Browser;
+using Costco.Utilities.FileReader.Models;
+using Costco.Web.Pages;
 
 namespace Costco.Tests
 {
-    public class LocationsTests
+    public class LocationsTests : IClassFixture<TestFixture>
     {
         TestFixture fixture;
 
@@ -14,7 +16,9 @@ namespace Costco.Tests
         [Fact]
         public void SetWarehouseAsMyWarehouseTest()
         {
-
+            LocationsPage locationsPage = new();
+            BrowserFactory.Browser.GoToUrl(locationsPage.Url);
+            //locationsPage.SearchForWarehouse();
         }
 
         [Fact]
