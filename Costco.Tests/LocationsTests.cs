a46f6a2c31cf8c3bf64c0963b3bbba45ab006934
Vasilii Denisov <v.denisov27@gmail.com>
2023-07-14 14:30:23 +0400

    Added Locations model and test class

---
+++ Costco.Tests/LocationsTests.cs
@@ -0,0 +1,32 @@
+﻿using Costco.Core.Browser;
+
+namespace Costco.Tests
+{
+    public class LocationsTests
+    {
+        TestFixture fixture;
+
+        public LocationsTests(TestFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void SetWarehouseAsMyWarehouseTest()
+        {
+
+        }
+
+        [Fact]
+        public void ViewStoreDetailsTest()
+        {
+
+        }
+
+        [Fact]
+        public void ChangeDeliveryLocationTest()
+        {
+
+        }
+    }
+}
---
+++ Costco.Utilities/FileReader/Models/LocationsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Costco.Utilities.FileReader.Models
+{
+    public class LocationsModel
+    {
+        public string Warehouse { get; init; }
+        public string DeliveryLocation { get; init; }
+    }
+}
