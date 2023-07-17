4484ec09b465c1f1d4a30a09b890b95e4512ac97
Filipp <ardos127@gmail.com>
2023-07-17 13:51:39 +0300

    Stabisized tests, added another waiter

--- Costco.Core/Browser/Waiters.cs
+++ Costco.Core/Browser/Waiters.cs
@@ -8,5 +8,7 @@ namespace Costco.Core.Browser
         public static void WaitForPageLoad() => BrowserFactory.Browser.Waiter(TestSettings.TimeOuts).Until(condition => BrowserFactory.Browser.ExecuteScript("return document.readyState").Equals("complete"));
 
         public static void WaitForCondition(Func<bool> condition, int seconds) => BrowserFactory.Browser.Waiter(seconds).Until(x => condition.Invoke());
+
+        public static void WaitUntilElementExists(By locator, int seconds) => BrowserFactory.Browser.Waiter(seconds).Until(ExpectedConditions.ElementExists(locator));
     }
 }
--- Costco.Tests/CostcoProductPageTestSuite.cs
+++ Costco.Tests/CostcoProductPageTestSuite.cs
@@ -1,17 +1,14 @@
 using Costco.Core.Browser;
 using Costco.Utilities.FileReader.Models;
-using Costco.Utilities.Logger;
-using Costco.Utilities.Screenshotter;
 using Costco.Web.Pages;
-using OpenQA.Selenium.Support.UI;
 
 namespace Costco.Tests
 {
-    public class CostcoProductPageTestSuite : IClassFixture<TestFixture>
+    public class CostcoProductPageTestSuite : BaseTest, IClassFixture<TestFixture>
     {
         TestFixture fixture;
 
-        public CostcoProductPageTestSuite(TestFixture fixture)
+        public CostcoProductPageTestSuite(TestFixture fixture, ITestOutputHelper output): base(output)
         {
             this.fixture = fixture;
         }
@@ -19,83 +16,53 @@ namespace Costco.Tests
         [Fact]
         public void ZeroProductsReturnError()
         {
-            try 
-            {
-                ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[0]);
+            ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[0]);
 
-                BrowserFactory.Browser.GoToUrl(productPage.Url);
-                Waiters.WaitForCondition(productPage.QuantityInput.IsDisplayed, 30);
-                productPage.InputProductAmount("0");
-                productPage.AddToCartButton.Click();
-                Waiters.WaitForCondition(productPage.ErrorMessageBelowInput.IsDisplayed, 30);
+            BrowserFactory.Browser.GoToUrl(productPage.Url);
+            Waiters.WaitForCondition(productPage.QuantityInput.IsDisplayed, 30);
+            productPage.InputProductAmount("0");
+            productPage.AddToCartButton.Click();
+            Waiters.WaitUntilElementExists(productPage.ErrorMessageBelowInputPath, 30);
 
-                Assert.Contains("Quantity must be 1 or more to add to cart.", productPage.GetErrorText());
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Test failed, exception {ex.Message}");
-                Screenshotter.TakeScreenshot(Browser.Driver); 
-                throw;
-            }
+            Assert.Contains("Quantity must be 1 or more to add to cart.", productPage.GetErrorText());
         }
 
         [Fact]
         public void OverLimitProductsReturnError()
         {
-            try
-            {
-                ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[1]);
-                string lowCutoff = "Limit ";
-                string highCutoff = " per member";
+            ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[1]);
+            string lowCutoff = "Limit ";
+            string highCutoff = " per member";
 
-                BrowserFactory.Browser.GoToUrl(productPage.Url);
-                Waiters.WaitForCondition(productPage.PromotionalText.IsDisplayed, 30);
-                string promoTextMaxQuantity = productPage.PromotionalText.Text;
+            BrowserFactory.Browser.GoToUrl(productPage.Url);
+            Waiters.WaitForCondition(productPage.PromotionalText.IsDisplayed, 30);
+            string promoTextMaxQuantity = productPage.PromotionalText.Text;
+            promoTextMaxQuantity = promoTextMaxQuantity.Substring(
+                promoTextMaxQuantity.IndexOf(lowCutoff) + lowCutoff.Length,
+                promoTextMaxQuantity.IndexOf(highCutoff) - promoTextMaxQuantity.IndexOf(lowCutoff) - lowCutoff.Length);
+            Waiters.WaitForCondition(productPage.QuantityInput.IsDisplayed, 30);
+            productPage.InputProductAmount((Int32.Parse(promoTextMaxQuantity) + 1).ToString());
+            productPage.AddToCartButton.Click();
+            productPage.AddToCartButton.Click();
+            Waiters.WaitUntilElementExists(productPage.ErrorMessageBelowInputPath, 30);
 
-                promoTextMaxQuantity = promoTextMaxQuantity.Substring(
-                    promoTextMaxQuantity.IndexOf(lowCutoff) + lowCutoff.Length,
-                    promoTextMaxQuantity.IndexOf(highCutoff) - promoTextMaxQuantity.IndexOf(lowCutoff) - lowCutoff.Length);
-
-                Waiters.WaitForCondition(productPage.QuantityInput.IsDisplayed, 30);
-
-                productPage.InputProductAmount((Int32.Parse(promoTextMaxQuantity) + 1).ToString());
-
-                productPage.AddToCartButton.Click();
-                Waiters.WaitForCondition(productPage.ErrorMessageBelowInput.IsEnabled, 30);
-
-                Assert.Contains($"Item {productPage.ItemNumber} has a maximum order quantity of {promoTextMaxQuantity}", 
-                    productPage.GetErrorText());
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Test failed, exception {ex.Message}");
-                Screenshotter.TakeScreenshot(Browser.Driver);
-                throw;
-            }
+            Assert.Contains($"Item {productPage.ItemNumber.Text} has a maximum order quantity of {promoTextMaxQuantity}", 
+                productPage.GetErrorText());
         }
 
         [Fact]
         public void OverMaximumProductReturnError()
         {
-            try
-            {
-                ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[2]);
+            ProductPage productPage = new(((ProductPageTestDataModel)fixture.testData).ProductPageUrl[2]);
 
-                BrowserFactory.Browser.GoToUrl(productPage.Url);
-                Waiters.WaitForCondition(productPage.QuantityInput.IsDisplayed, 30);
-                productPage.InputProductAmount("999");
-                productPage.PlusStepper.Click();
-                productPage.AddToCartButton.Click();
-                Waiters.WaitForCondition(productPage.ErrorMessageInsideInput.IsDisplayed, 30);
+            BrowserFactory.Browser.GoToUrl(productPage.Url);
+            Waiters.WaitForCondition(productPage.QuantityInput.IsDisplayed, 30);
+            productPage.InputProductAmount("999");
+            productPage.PlusStepper.Click();
+            productPage.AddToCartButton.Click();
+            Waiters.WaitForCondition(productPage.ErrorMessageInsideInput.IsDisplayed, 30);
 
-                Assert.Equal("Please enter no more than 3 characters.", productPage.GetInputFieldErrorText());
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"Test failed, exception {ex.Message}");
-                Screenshotter.TakeScreenshot(Browser.Driver);
-                throw;
-            }
+            Assert.Equal("Please enter no more than 3 characters.", productPage.GetInputFieldErrorText());
         }
     }
 }
