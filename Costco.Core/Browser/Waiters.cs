ed9e0764594671a988a130ffa64eaccbfbc24cb7
Uladzislau <tarasenko_vlad@inbox.ru>
2023-07-17 18:39:42 +0400

    Placed WaitUntilElementExists back.

--- Costco.Core/Browser/Waiters.cs
+++ Costco.Core/Browser/Waiters.cs
@@ -1,9 +1,14 @@
-﻿namespace Costco.Core.Browser
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace Costco.Core.Browser
 {
     public static class Waiters
     {
         public static void WaitForPageLoad() => BrowserFactory.Browser.Waiter(TestSettings.TimeOuts).Until(condition => BrowserFactory.Browser.ExecuteScript("return document.readyState").Equals("complete"));
 
         public static void WaitForCondition(Func<bool> condition, int seconds) => BrowserFactory.Browser.Waiter(seconds).Until(x => condition.Invoke());
+
+        public static void WaitUntilElementExists(By locator, int seconds) => BrowserFactory.Browser.Waiter(seconds).Until(ExpectedConditions.ElementExists(locator));
     }
 }
