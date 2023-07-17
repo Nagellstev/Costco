e8562e1a8063907e9d65b52e81a6b1726a3c66b4
Vasilii Denisov <v.denisov27@gmail.com>
2023-07-17 14:08:56 +0400

    Wrote remaining tests

--- Costco.Tests/LocationsTests.cs
+++ Costco.Tests/LocationsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Costco.Core.Browser;
 using Costco.Utilities.FileReader.Models;
+using Costco.Utilities.Logger;
 using Costco.Web.Pages;
 
 namespace Costco.Tests
@@ -20,10 +21,11 @@ namespace Costco.Tests
 
             LocationsPage locationsPage = new();
             BrowserFactory.Browser.GoToUrl(locationsPage.Url);
+            Waiters.WaitForCondition(() => locationsPage.WarehouseSearch.IsEnabled(), 10);
             locationsPage.SearchForWarehouse(warehouse);
-            Waiters.WaitForCondition(() => locationsPage.SetAsMyWarehouseButton.IsDisplayed(), 5);
+            Waiters.WaitForCondition(() => locationsPage.SetAsMyWarehouseButton.IsEnabled(), 10);
             locationsPage.SetAsMyWarehouse();
-            Waiters.WaitForCondition(() => locationsPage.IsWarehouseSet(warehouse), 5);
+            Waiters.WaitForCondition(() => locationsPage.IsWarehouseSet(warehouse), 10);
 
             Assert.True(locationsPage.IsWarehouseSet(warehouse));
         }
@@ -31,13 +33,36 @@ namespace Costco.Tests
         [Fact]
         public void ViewStoreDetailsTest()
         {
+            string warehouse = ((LocationsModel)fixture.testData).Warehouse;
 
+            LocationsPage locationsPage = new();
+            BrowserFactory.Browser.GoToUrl(locationsPage.Url);
+            Waiters.WaitForCondition(() => locationsPage.WarehouseSearch.IsEnabled(), 10);
+            locationsPage.SearchForWarehouse(warehouse);
+            Waiters.WaitForCondition(() => locationsPage.IsResultFound(warehouse), 10);
+            string nameOnLocationsPage = locationsPage.GetWarehouseName();
+            string addressOnLocationsPage = locationsPage.GetWarehouseAddress();
+            locationsPage.ClickStoreDetails();
+            WarehousePage warehousePage = new();
+            Waiters.WaitForCondition(() => warehousePage.Name.IsEnabled(), 10);
+            string nameOnWarehousePage = warehousePage.GetName();
+            string addressOnWarehousePage = warehousePage.GetAddress();
+
+            Assert.True(nameOnLocationsPage == nameOnWarehousePage &&
+                addressOnLocationsPage == addressOnWarehousePage);
         }
 
         [Fact]
         public void ChangeDeliveryLocationTest()
         {
+            string deliveryLocation = ((LocationsModel)fixture.testData).DeliveryLocation;
 
+            LocationsPage locationsPage = new();
+            BrowserFactory.Browser.GoToUrl(locationsPage.Url);
+            locationsPage.LocationsBlock.SetDeliveryLocation(deliveryLocation);
+            Waiters.WaitForCondition(() => locationsPage.IsDeliveryLocationSet(deliveryLocation), 10);
+
+            Assert.True(locationsPage.IsDeliveryLocationSet(deliveryLocation));
         }
     }
 }
