73e1fc1967be33f01a9341e3a10fdb11bb57244b
Uladzislau <tarasenko_vlad@inbox.ru>
2023-07-26 12:24:59 +0400

    Add MainPageSteps and LoginPageSteps classes

--- Costco.Web/Pages/MainPage.cs
+++ Costco.Web/Pages/MainPage.cs
@@ -18,19 +18,6 @@ namespace Costco.Web.Pages
         public Button AccountButton => new Button(AccountButtonLocator);
         public SearchBlock SearchBlock => new SearchBlock(SearchBlockLocator);
 
-        public void NavigateToLoginPage()
-        {
-            Waiters.WaitForCondition(() => SignInButton.IsDisplayed(), 5);
-            BrowserFactory.Browser.MoveMouseToElement(BrowserFactory.Browser.FindElement(SignInButtonLocator));
-            SignInButton.Click();
-            Waiters.WaitForCondition(() => new LoginPage().PasswordInputField.IsDisplayed(), 10);
-        }
-
-        public bool VerifyUserIsLoggedIn()
-        {
-            Waiters.WaitForCondition(() => AccountButton.IsDisplayed(), 10);
-            return AccountButton.IsDisplayed();
-        }
 
         public void SearchFieldInput(string input)
         {
