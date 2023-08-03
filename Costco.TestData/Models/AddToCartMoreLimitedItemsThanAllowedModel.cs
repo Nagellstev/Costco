2fe9375e80e9218c6462a373bdcb6562c0462663
Filipp <ardos127@gmail.com>
2023-08-03 16:46:23 +0300

    Moved TestData to GetEnuminator

--- Costco.TestData/Models/AddToCartMoreLimitedItemsThanAllowedModel.cs
+++ Costco.TestData/Models/AddToCartMoreLimitedItemsThanAllowedModel.cs
@@ -7,18 +7,13 @@ namespace Costco.TestData.Models
     {
         private DataContainer _container;
 
-        private IEnumerable<object[]> Data()
+        public IEnumerator<object[]> GetEnumerator()
         {
             FileReader reader = new();
             _container = (DataContainer)reader.Read(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
                 "..", "..", "..", "..", "Costco.TestData", "TestData", "AddToCartMoreLimitedItemsThanAllowedTestData.json"),
                 typeof(DataContainer).AssemblyQualifiedName);
-            return _container.TestData;
-        }
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            return Data().GetEnumerator();
+            return _container.TestData.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
