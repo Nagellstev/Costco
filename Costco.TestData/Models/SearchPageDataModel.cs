2fc929a058b518e8d3eb802ea77438c25f31cbbf
PavelD <nagellstev@ya.ru>
2023-08-13 15:33:13 +0400

    changed tests according to the latest DDT approach

---
+++ Costco.TestData/Models/SearchPageDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Costco.TestData.Models
+{
+    public class SearchPageDataModel
+    {
+        public string SearchExistingItem { get; init; }
+        public string SearchExistingItemResult { get; init; }
+        public string SearchSenselessLine { get; init; }
+        public string SearchSenselessLineResult { get; init; }
+        public string SearchExistingItemAndFilterByPriceSearch { get; init; }
+        public string SearchExistingItemAndFilterByPriceFilter { get; init; }
+    }
+}
