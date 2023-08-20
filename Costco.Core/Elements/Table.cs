a2acd94a79dbdb0c78009051dff54cacffbd3bdc
Vasilii Denisov <v.denisov27@gmail.com>
2023-08-20 21:37:17 +0400

    Added table element. Added waiter. Increases tests stability

---
+++ Costco.Core/Elements/Table.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace Costco.Core.Elements
+{
+    public class Table : BaseElement
+    {
+        public Table(By locator) : base(locator) { }
+        public Table(IWebElement element) : base(element) { }
+
+        public bool IsEmpty()
+        {
+            IReadOnlyCollection<IWebElement> rows = OriginalWebElement.FindElements(By.TagName("tr"));
+            return rows.Count == 0;
+        }
+
+        public bool IsNotEmpty()
+        {
+            return !IsEmpty();
+        }
+    }
+}
