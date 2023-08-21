b744f3d493fb8815b52962f06e16ceb90940bf1c
Filipp <ardos127@gmail.com>
2023-08-21 14:38:45 +0300

    Initial version, no screenshots, minimal logging

---
+++ Costco.BDTTests/StepDefinitions/ProductPageStepDefinitions.cs
@@ -0,0 +1,120 @@
+﻿using Costco.Web.Pages;
+using Costco.Core.Browser;
+using Costco.Utilities.Screenshoter;
+using Costco.Utilities.Logger;
+using TechTalk.SpecFlow;
+
+namespace Costco.BDTTests.StepDefinitions
+{
+    [Binding]
+    public sealed class ProductPageStepDefinitions
+    {
+        private ScenarioContext _scenarioContext;
+        private ProductPage _productPage;
+        string promoTextMaxQuantity;
+
+        public ProductPageStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _productPage = new();
+            BrowserFactory.Browser.Maximize();
+            Logger.Information("Setup complete.");
+        }
+
+        [BeforeFeature]
+        public static void Setup()
+        {
+            Logger.Init("Costco", TestSettings.LoggerPath);
+            Screenshoter.Init(TestSettings.ScreenshotPath);
+            Logger.Information("Pre-run setup complete.");
+        }
+
+        [AfterScenario]
+        public void Cleanup()
+        {
+            BrowserFactory.CleanUp();
+            Logger.Information("Cleaning up.");
+        }
+
+        [Given(@"I opened the product page (.*)")]
+        public void OpenPage(string url)
+        {
+            BrowserFactory.Browser.GoToUrl(url);
+        }
+
+        [Given("I want to order (.*) items")]
+        public void InputDesiredQuantity(int quantity)
+        {
+            _scenarioContext["OrderQuantity"] = quantity;
+        }
+
+        [Given("I see the maximum number of limited items I can order")]
+        public void GetMaximumLimitedItemsAllowed()
+        {
+            string lowCutoff = "Limit ";
+            string highCutoff = " per member";
+
+            Waiters.WaitForCondition(_productPage.PromotionalText.IsDisplayed);
+            promoTextMaxQuantity = _productPage.PromotionalText.Text;
+            promoTextMaxQuantity = promoTextMaxQuantity.Substring(
+                promoTextMaxQuantity.IndexOf(lowCutoff) + lowCutoff.Length,
+                promoTextMaxQuantity.IndexOf(highCutoff) - promoTextMaxQuantity.IndexOf(lowCutoff) - lowCutoff.Length);
+            _scenarioContext["OrderQuantity"] = promoTextMaxQuantity;
+        }
+
+        [When(@"I add (.*) more to the desired amount")]
+        public void InputZeroProductAmount(int amount)
+        {
+            _scenarioContext["OrderQuantity"] = Int32.Parse((string)_scenarioContext["OrderQuantity"]) + amount;
+        }
+
+        [When(@"I enter the desired amount to the product amount field")]
+        public void InputZeroProductAmount()
+        {
+            Waiters.WaitForCondition(_productPage.QuantityInput.IsDisplayed);
+            _productPage.QuantityInput.Clear();
+            _productPage.QuantityInput.SendKeys(_scenarioContext["OrderQuantity"].ToString());
+        }
+
+        [When("I press add to card button")]
+        public void PressAddToCart()
+        {
+            _productPage.AddToCartButton.Click();
+        }
+
+        [When("I press plus stepper")]
+        public void PressPlusOneStepper()
+        {
+            Waiters.WaitForCondition(_productPage.PlusStepper.IsEnabled);
+            _productPage.PlusStepper.Click();
+        }
+
+        [Then("Error (.*) is displayed below the input field")]
+        public void GetBelowFieldErrorText(string error)
+        {
+            Waiters.WaitUntilElementExists(_productPage.ErrorMessageBelowInputPath);
+            switch (error)
+            {
+                case "Item ... has a maximum order quantity of ...":
+                    {
+                        Assert.Contains($"Item {_productPage.ItemNumber.Text} has a maximum order quantity of {promoTextMaxQuantity}",
+                            _productPage.ErrorMessageBelowInput.Text.Trim());
+                        break;
+                    }
+
+                default:
+                    {
+                        Assert.Contains(error, _productPage.ErrorMessageBelowInput.Text.Trim());
+                        break;
+                    }
+            }
+        }
+
+        [Then("Error (.*) is displayed in the input field")]
+        public void GetInputFieldErrorText(string error)
+        {
+            Waiters.WaitForCondition(_productPage.ErrorMessageInsideInput.IsDisplayed);
+            Assert.Contains(error, _productPage.ErrorMessageInsideInput.Text.Trim());
+        }
+    }
+}
--- Costco.Web/Steps/ProductPageSteps.cs
+++ Costco.Web/Steps/ProductPageSteps.cs
@@ -1,5 +1,4 @@
 ﻿using Costco.Core.Browser;
-using Costco.Utilities.Logger;
 using Costco.Web.Pages;
 
 namespace Costco.Web.Steps
@@ -17,19 +16,16 @@ namespace Costco.Web.Steps
         {
             Waiters.WaitForCondition(_productPage.QuantityInput.IsDisplayed);
             _productPage.QuantityInput.Clear();
-            Logger.Information($"Inputting {amount} in product order quantity field.");
             _productPage.QuantityInput.SendKeys(amount.ToString());
         }
 
         public void PressAddToCart()
         {
-            Logger.Information($"Pressing \"Add to card\" button.");
             _productPage.AddToCartButton.Click();
         }
 
         public void PressPlusOneStepper(int quantity)
         {
-            Logger.Information($"Pressing + stepper {quantity} times.");
             for (int i = 0; i < quantity; i++)
             {
                 Waiters.WaitForCondition(_productPage.PlusStepper.IsEnabled);
@@ -44,21 +40,18 @@ namespace Costco.Web.Steps
             promoTextMaxQuantity = promoTextMaxQuantity.Substring(
                 promoTextMaxQuantity.IndexOf(lowCutoff) + lowCutoff.Length,
                 promoTextMaxQuantity.IndexOf(highCutoff) - promoTextMaxQuantity.IndexOf(lowCutoff) - lowCutoff.Length);
-            Logger.Information($"Maximum number of items allowed is determined to be {promoTextMaxQuantity}.");
             return Int32.Parse(promoTextMaxQuantity);
         }
 
         public string GetErrorText()
         {
             Waiters.WaitUntilElementExists(_productPage.ErrorMessageBelowInputPath);
-            Logger.Information($"Received error \"{_productPage.ErrorMessageBelowInput.Text.Trim()}\".");
             return _productPage.ErrorMessageBelowInput.Text.Trim();
         }
 
         public string GetInputFieldErrorText()
         {
             Waiters.WaitForCondition(_productPage.ErrorMessageInsideInput.IsDisplayed);
-            Logger.Information($"Received error \"{_productPage.ErrorMessageInsideInput.Text.Trim()}\".");
             return _productPage.ErrorMessageInsideInput.Text.Trim();
         }
 
