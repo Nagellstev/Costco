cd022041ebeb4e03b08f782e6504f13ffacd4173
Vasilii Denisov <v.denisov27@gmail.com>
2023-08-23 15:04:56 +0400

    Added WebDriverManager drivers configuration

--- Costco.Core/Browser/FirefoxBrowser.cs
+++ Costco.Core/Browser/FirefoxBrowser.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
 
 namespace Costco.Core.Browser
 {
@@ -7,6 +9,7 @@ namespace Costco.Core.Browser
     {
         public IWebDriver GetDriver()
         {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
             var options = new FirefoxOptions();
             var service = FirefoxDriverService.CreateDefaultService();
             var driver = new FirefoxDriver(service, options, TimeSpan.FromSeconds(TestSettings.TimeOuts));
