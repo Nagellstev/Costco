f9d71436a32de01a95178eb702324cc3b4694335
Uladzislau <tarasenko_vlad@inbox.ru>
2023-09-11 20:59:05 +0400

    Add VladTests class and wrote api tests

---
+++ Reqres.Tests/VladTests.cs
@@ -0,0 +1,71 @@
+﻿using Reqres.Core;
+using FluentAssertions;
+using System.Net;
+
+namespace Reqres.Tests
+{
+    public class VladTests : BaseTest, IClassFixture<TestFixture>
+    {
+        TestFixture fixture;
+
+        public VladTests(TestFixture fixture, ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void GetNonExistentUser()
+        {
+            //Arrange
+            Client client = builder.GetClient();
+            string url = "api/users/23";
+
+            //Action
+            var response = client.Get(url);
+
+            //Assert
+            Assert.Multiple(
+                () => response.StatusCode.Should().Be(HttpStatusCode.NotFound),
+                () => response.Content.Should().Be("{}"));
+        }
+
+        [Fact]
+        public void GetNonExistentResource()
+        {
+            //Arrange
+            Client client = builder.GetClient();
+            string url = "api/unknown/23";
+
+            //Action
+            var response = client.Get(url);
+
+            //Assert
+            Assert.Multiple(
+                () => response.StatusCode.Should().Be(HttpStatusCode.NotFound),
+                () => response.Content.Should().Be("{}"));
+        }
+
+        [Fact]
+        public void UpdateUsersName()
+        {
+            //Arrange
+            Client client = builder.GetClient();
+            string url = "api/users/2";
+            string body = "{" +
+                "\"name\": \"morpheus\", " +
+                "\"job\": \"zion resident\"" +
+            "}";
+
+            //Action
+            var response = client.Put(url, body);
+
+            //Assert
+            Assert.Multiple(
+                () => response.StatusCode.Should().Be(HttpStatusCode.OK),
+                () => response.Content.Should().ContainAll(new string[] {
+                        "\"name\":\"morpheus\"",
+                        "\"job\":\"zion resident\"",
+                        "updatedAt"}));
+        }
+}
+}
